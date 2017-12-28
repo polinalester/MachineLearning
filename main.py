d1f9cd8b59db5db33c55e03ec9d2f897225067a0
polinalester <epikh1gh@yandex.ru>
2017-12-28 23:01:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from scipy.optimize import minimize
 
 filename="transport_data.csv"
+print("Importing data from input file..")
 with open(filename,"r",newline="") as file:
     reader=csv.reader(file)
     rownum=0
@@ -12,7 +14,7 @@ with open(filename,"r",newline="") as file:
     for row in reader:
         a.append(row)
         rownum+=1
-
+print("Imported.")
 a = np.delete(a,2,1)
 
 max_longitude = max(a[:,0])
@@ -22,37 +24,39 @@ min_longitude = min(a[:,0])
 min_latitude = min(a[:,1])
 min_time = min(a[:,2])
 
+known = []
 train = []
 test = []
+task = []
 
+print("Dividing data...")
 for i in range(len(a)):
         if a[i][3] == '?':
-            test.append(a[i])
-        elif a[i][3] != '-':
-            train.append(a[i])
+            task.append(a[i])
+        elif a[i][3] != '-': # 40 000 records
+            known.append(a[i])
+
+training_percent = 0.01
+record_number = len(known)
+test = np.array(known)[int(record_number * training_percent) : record_number]
+train = np.array(known)[0:int(record_number * training_percent)]
 
 x_train = np.array(train)[:, 0:3]
-y_train_category = np.array(train)[:, 3]
-y_train_size = len(y_train_category)
-y_train = np.zeros((y_train_size, 3))
-
-for i in range(len(y_train_category)):
-    ind = y_train_category[i]
-    ind = ind.astype(np.int)
-    y_train[i][ind] = 1
-print(y_train[9])
-print(y_train_category[9])
+y_train = np.array(train)[:, 3]
 
 x_test = np.array(test)[:, 0:3]
-y_test = []
+y_test = np.array(test)[:, 3]
+
+x_task = np.array(task)[:, 0:3]
+y_task = []
 
 x_train = x_train.astype(np.float)
-
 x_test = x_test.astype(np.float)
-
+x_task = x_task.astype(np.float)
+print("Divided.")
 #x_train[0] = np.divide(x_train[0], max_longitude)
 
-
+print("Normalizing data...")
 max_longitude = max_longitude.astype(np.float)
 max_latitude = max_latitude.astype(np.float)
 max_time = max_time.astype(np.float)
@@ -73,75 +77,92 @@ for i in range(len(x_test)):
         x_test[i][0] = (x_test[i][0]-min_longitude)/delta_longitude
         x_test[i][1] = (x_test[i][1]-min_latitude)/delta_latitude
         x_test[i][2] = (x_test[i][2]-min_time)/delta_time
+for i in range(len(x_task)):
+        x_task[i][0] = (x_task[i][0]-min_longitude)/delta_longitude
+        x_task[i][1] = (x_task[i][1]-min_latitude)/delta_latitude
+        x_task[i][2] = (x_task[i][2]-min_time)/delta_time
+print("Normalized.")
+x_first_train = x_train
+y_first_train = y_train
+for i in range(len(x_first_train)):
+    if y_train[i] == 0:
+        y_first_train[i] = 1
+    else:
+        y_first_train[i] = -1
 
-layers_number = 3
+print("Building dividing hyperplain...")
 
-w1 = np.array([
-  [0.3, 0.2, 0.1],
-  [0.3, 0.6, 0.5],
-  [0.1, 0.1, 0.4]
-])
-w2 = np.array([
-  [0.4, 0.3, 0.1],
-  [0.6, 0.2, 0.6],
-  [0.2, 0.1, 0.3]
-])
-for i in range(len(w1)):
-    for j in range(len(w1[i])):
-        w1[i][j] = random.uniform(-0.5,0.5)
-        
-for i in range(len(w2)):
-    for j in range(len(w2[i])):
-        w2[i][j] = random.uniform(-0.5,0.5)
+n = len(train)
+#0<=lambdai<=c, 1<=i<=n
+#summ(i=1...n)lambdai*ci=0
+def func(lmbda):
+    result = 0
+    for i in range(n):
+        for j in range(n):
+            temp = lmbda[i]*lmbda[j]
+            #temp = temp * y_first_train[i]
+            #temp = temp * y_first_train[j]
+            x_dot = np.dot(x_first_train[i],x_first_train[j])
+            temp = temp * x_dot
+            result = result + temp
+            #result=result+lmbda[i]*lmbda[j]*y_first_train[i]*y_first_train[j]*np.dot(x_first_train[i],x_first_train[j])
+    result = result/2
+    for i in range(n):
+        result = result - lmbda[i]
+    return result
 
-for i in range(len(w2)):
-    for j in range(len(w2[i])):
-        print(w2[i][j])
-        
-#w3 = np.array([0.2,0.4,0.6])
+def func_deriv(lmbda):
+    result = 0
+    for i in range(n):
+        for j in range(n):
+            temp = lmbda[i]+lmbda[j]
+            #temp = temp * y_first_train[i]
+            #temp = temp * y_first_train[j]
+            x_dot = np.dot(x_first_train[i],x_first_train[j])
+            temp = temp * x_dot
+            result = result + temp
+            #result=result+lmbda[i]*lmbda[j]*y_first_train[i]*y_first_train[j]*np.dot(x_first_train[i],x_first_train[j])
+    result = result/2
+    return result
 
-def activation_func(x):
-    fx = 1/(1+np.exp(-x))
-    return fx
+w = 0
+b = 0
+C = 1
+lambda0 = [0] * n #initial point
+print("Initialazing bounds & constraints...")
+bnds = [[0,C]] * n
+cons = ({'type' : 'eq',
+         'fun' : lambda lmbda : np.dot(lmbda,y_first_train),
+         'jac' : lambda lmbda : np.sum(y_first_train)})
+#res = minimize(func, lambda0, method = 'SLSQP', bounds = bnds, constraints = cons)
 
-def activation_func_der(x):
-    dfx = activation_func(x)*(1-activation_func(x))
-    return dfx
+print("Minimizing Lagrangian...")
 
-def calculate_y(x, w1, w2):
-    #print(x)
-    z = [0] * (len(x))
-    for i in range(len(z)):
-        for j in range(len(x)):
-            #print(x[j]*w1[j][i])
-            z[i]=z[i]+x[j]*w1[j][i]
-        z[i]=activation_func(z[i])
-    #print(z)
-    y = [0] * (len(z))
-    for i in range(len(y)):
-        for j in range(len(z)):
-            y[i]=y[i]+z[j]*w2[j][i]
-        y[i]=activation_func(y[i])
-    return y
+res = minimize(func, lambda0, method = 'SLSQP', bounds = bnds)
 
-#learning
-alpha = 0.5 #learning speed
-calc_error = 0
-n = 0
-while n < len(x_train):
-    calc_error = 0
-    y = calculate_y(x_train[n],w1,w2)
-    for j in range(len(y_train[0])):
-        calc_error = calc_error + (y[j] - y_train[j])
-        
+print("Minimized.")
 
-#calculating
-for j in range(len(x_test)):
-    y = calculate_y(x_test[j],w1,w2)
-    y_max_index = y.index(max(y))
-    y_test.append(y_max_index)
+print("Calculating wx+b=0...")
+print("Calculating w...")
+for i in range(n):
+    w = w + lmbda[i]*y[i]*x_train[i]
+print("Calculated.")
+print("Calculating b...")
+b = np.dot(w,x[0]) - y[0]
+print("Calculated.")
+
+print("Classifying data...")
+
+def classify(x):
+    return np.sign(np.dot(w,x)+b)
+
+print("Classified.")
+
+print("Writing data to output file...")
 
 with open("result.txt","w",newline="") as file:
     for i in range(len(y_test)):
-        file.write(str(y_test[i]))
+        file.write(y_test[i])
         file.write("\n")
+
+print("Finished.")
